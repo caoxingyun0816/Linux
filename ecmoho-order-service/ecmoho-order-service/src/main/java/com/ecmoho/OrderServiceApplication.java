88cfe20635b34485a506653c88eb71ca55aefd22
caoxingyun <cxydy0816>
2019-07-18 19:33:58 +0800

    int

---
+++ ecmoho-order-service/ecmoho-order-service/src/main/java/com/ecmoho/OrderServiceApplication.java
@@ -0,0 +1,21 @@
+package com.ecmoho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+
+/**
+ * Create by HuangJin 0n 2019/7/18.
+ */
+@RefreshScope
+@SpringBootApplication
+@EnableFeignClients
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
